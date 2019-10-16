fb53145279610511f49170329513d183b79fe00b
nolleto <felipenolletonascimento@gmail.com>
2019-10-16 18:17:54 -0300

    Refactor application layout and user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by!(username: params[:username])
+    @posts = @user.posts.order('created_at DESC')
+    @comments_count = User.joins(posts: :comments)
+                          .where(id: @user.id)
+                          .count(:comments) 
   end
 end
