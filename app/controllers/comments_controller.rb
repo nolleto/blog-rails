27a58fa6e2213519c37b2ddbf0cb06d03e66dc00
nolleto <felipenolletonascimento@gmail.com>
2020-01-07 09:30:25 -0300

    Fix delete comment without permission

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,14 +11,18 @@ class CommentsController < ApplicationController
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-    @comment.destroy
 
+    if @comment.user != current_user
+      return render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
+    end
+
+    @comment.destroy
     redirect_to post_path(@post)
   end
 
   private
 
-  def comment_params 
+  def comment_params
     params[:comment].permit(:body).merge(user: current_user)
   end
 end
